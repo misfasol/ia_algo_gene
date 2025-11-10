a4ac16a0a16f2fa0faf042bebdb16b456e5238ea
misfasol <gabriel.schlagenhaufer@gmail.com>
2025-11-10 13:26:56 -0300

    baseline feito

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
-dados_treino = pd.read_csv("dados/mnist_train.csv")
-dados_teste = pd.read_csv("dados/mnist_test.csv")
-print(dados_treino.head())
-print(dados_teste.head())
+from time import time
+
+treino = pd.read_csv("dados/mnist_train.csv")
+# print(treino.head())
+treino_labels = treino["label"]
+# print(treino_labels.head())
+treino_dados = treino.drop("label", axis=1)
+# print(treino_dados.head())
+
+teste = pd.read_csv("dados/mnist_test.csv")
+# print(teste.head())
+teste_labels = teste["label"]
+# print(teste_labels)
+teste_dados = teste.drop("label", axis=1)
+# print(teste_dados)
 
 def tree_ga():
     pass
@@ -13,7 +24,16 @@ def tree_wrapper():
     pass
 
 def tree_baseline():
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(random_state=0)
+    a = time()
+    clf.fit(treino_dados, treino_labels)
+    tempo_treino = time() - a
+    acuracia = clf.score(teste_dados, teste_labels) * 100
+    print("Baseline")
+    print(f"{acuracia = }")
+    print(f"porcentagem_features = 100")
+    print(f"{tempo_treino = }")
+    print(f"tempo_busca_features = 0")
 
 def main():
     tree_ga()
