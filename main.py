61e7d9757df4a817eef8be2f5d610fbe3af56f4b
misfasol <gabriel.schlagenhaufer@gmail.com>
2025-11-10 11:48:49 -0300

    mudei main e add tabela

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+
+dados_treino = pd.read_csv("dados/mnist_train.csv")
+dados_teste = pd.read_csv("dados/mnist_test.csv")
+print(dados_treino.head())
+print(dados_teste.head())
+
+def tree_ga():
+    pass
+
+def tree_wrapper():
+    pass
+
+def tree_baseline():
+    clf = DecisionTreeClassifier()
 
 def main():
-    X = [[0, 0], [1, 1]]
-    Y = [0, 1]
-    clf = DecisionTreeClassifier()
-    clf.fit(X, Y)
-    print(clf.predict([[2, 2]]))
-    print("oi")
+    tree_ga()
+    tree_wrapper()
+    tree_baseline()
 
 if __name__ == "__main__":
     main()
